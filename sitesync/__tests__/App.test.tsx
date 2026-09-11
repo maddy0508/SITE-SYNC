3ad13c554ac31b96d5f57c531ef6a50273a63503
maddy0508 <mckellarmaddy@gmail.com>
2026-09-11 16:57:36 +1000

    test(M1.5): mock native scanner modules in app smoke test

--- sitesync/__tests__/App.test.tsx
+++ sitesync/__tests__/App.test.tsx
@@ -4,6 +4,21 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
+
+jest.mock('react-native-vision-camera', () => ({
+  Camera: 'Camera',
+  useCameraDevice: () => null,
+  useCameraPermission: () => ({
+    hasPermission: false,
+    canRequestPermission: false,
+    requestPermission: jest.fn(),
+  }),
+}));
+
+jest.mock('react-native-vision-camera-barcode-scanner', () => ({
+  useBarcodeScannerOutput: () => ({}),
+}));
+
 import App from '../App';
 
 test('renders correctly', async () => {
