430d30693be2d0f69d2c68793b800858161188fb
maddy0508 <mckellarmaddy@gmail.com>
2026-09-12 09:24:58 +1000

    feat: export Supabase sync transport

--- sitesync/src/sync/index.ts
+++ sitesync/src/sync/index.ts
@@ -2,3 +2,4 @@ export * from './syncTransport';
 export * from './syncRetryPolicy';
 export * from './syncCommandRepository';
 export * from './syncWorker';
+export * from './supabaseSyncTransport';
